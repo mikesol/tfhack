f25a2cd2c317edc8ec121c6cbcbc4e0676750c74
Mike Solomon <mike@mikesolomon.org>
2018-06-19 19:25:29 +0300

    learns random numbers

--- src/App.vue
+++ src/App.vue
@@ -12,24 +12,42 @@ import * as tf from '@tensorflow/tfjs';
 import * as _ from 'lodash';
 
 // Define a model for linear regression.
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+//const model = tf.sequential();
+//model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
 // Prepare the model for training: Specify the loss and the optimizer.
-model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+//model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+  // Train a simple model:
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
+model.add(tf.layers.dense({units: 1, activation: 'linear'}));
+model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
+
+const xs = tf.randomNormal([100, 10]);
+const ys = tf.randomNormal([100, 1]);
 
 // Generate some synthetic data for training.
-const x_hack = _.range(50);
-const y_hack = x_hack.map(x => x + 1);
-const xs = tf.tensor2d(x_hack, [x_hack.length, 1]);
-const ys = tf.tensor2d(y_hack, [y_hack.length, 1]);
+//const x_hack = _.range(50);
+//const y_hack = x_hack.map(x => x + 1);
+//const xs = tf.tensor2d(x_hack, [x_hack.length, 1]);
+//const ys = tf.tensor2d(y_hack, [y_hack.length, 1]);
 xs.print();
 ys.print();
 // Train the model using the data.
-model.fit(xs, ys, {epochs: 10}).then(() => {
+//model.fit(xs, ys, {epochs: 10}).then(() => {
   // Use the model to do inference on a data point the model hasn't seen before:
-  model.predict(tf.tensor2d([5.015], [1, 1])).print();
-});
+//  model.predict(tf.tensor2d([5], [1, 1])).print();
+//});
+
+model.fit(xs, ys, {
+    epochs: 100,
+    callbacks: {
+      onEpochEnd: async (epoch, log) => {
+        console.log(`Epoch ${epoch}: loss = ${log.loss}`);
+      }
+    }
+  });
 
 function* generator() {
   let index = 0;
