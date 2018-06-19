c0e72f2c00d5e6e6f339aa9f3a342b5a8d19aa5d
Mike Solomon <mike@mikesolomon.org>
2018-06-19 19:10:05 +0300

    Tf hack

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,28 @@
 </template>
 
 <script>
+import * as tf from '@tensorflow/tfjs';
+import * as _ from 'lodash';
+
+// Define a model for linear regression.
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+// Prepare the model for training: Specify the loss and the optimizer.
+model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+// Generate some synthetic data for training.
+const x_hack = _.range(50);
+const y_hack = x_hack.map(x => x + 1);
+const xs = tf.tensor2d(x_hack, [x_hack.length, 1]);
+const ys = tf.tensor2d(y_hack, [y_hack.length, 1]);
+xs.print();
+ys.print();
+// Train the model using the data.
+model.fit(xs, ys, {epochs: 10}).then(() => {
+  // Use the model to do inference on a data point the model hasn't seen before:
+  model.predict(tf.tensor2d([5.015], [1, 1])).print();
+});
 export default {
   name: 'app',
   data () {
