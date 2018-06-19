253d96d73df36d07ea4f47f8ca28394d0681c765
Kimmo Sääskilahti <ksaaskil@gmail.com>
2018-06-19 19:04:55 +0300

    Add examples how to access properties in Vue controller.

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,49 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h1>{{ msg }} {{ counter }}</h1>
+    <h2>Functions can be invoked: {{ counterFromFunction() }}</h2>
+    <h3>Computed properties: {{ counterFromComputed }}</h3>
   </div>
 </template>
 
 <script>
+
+function* generator() {
+  let index = 0;
+  while(true) {
+    yield index++;
+  }
+}
+
+const gen = generator();
+
+let counter = 0;
+
+//let msg = 'This comes from Vue';
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Counter is at value:',
+      counter
     }
-  }
+  },
+  methods: {
+    counterFromFunction: function () {
+      return this.counter;
+    }
+  },
+  computed: {
+    counterFromComputed: function () {
+      return this.counter;
+    }
+  },
+  created() {
+    setInterval(() => {
+      this.counter = gen.next().value;
+    }, 1000);
+},
 }
 </script>
 
